b7e3fd4371889b0095fb2795e299366f1c0fe912
Wesley Adiel <wesleyadiel@hotmail.com>
2021-11-24 18:52:58 -0300

    First Commit

---
+++ NextSerie/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NextSerie.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NextSerie.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ValidarLogin(LoginModel model)
+        {
+            for (int i = 0; i<1000; i++)
+            {
+                Console.WriteLine("claaaa");
+            }
+
+            return null;
+        }
+    }
+}
